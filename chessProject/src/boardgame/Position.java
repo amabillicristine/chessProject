183aa7d02cb29a75a69242bdff74df0022f5df56
Amabilli Cristine Wizzentainer <115269283+amabillicristine@users.noreply.github.com>
2025-06-05 14:30:17 -0300

    Add Position class to represent board game positions

---
+++ chessProject/src/boardgame/Position.java
@@ -0,0 +1,40 @@
+package boardgame;
+
+public class Position {
+    private int row;
+    private int column;
+
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public Position() {
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Position{");
+        sb.append("row: ").append(row);
+        sb.append(", column: ").append(column);
+        sb.append('}');
+        return sb.toString();
+    }
+
+}
