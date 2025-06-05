6f8c568ed24ec8de49972c269dc941e58efa9573
Amabilli Cristine Wizzentainer <115269283+amabillicristine@users.noreply.github.com>
2025-06-05 15:59:24 -0300

    Add ChessMatch, ChessPiece, Color, and UI classes for chess game implementation

--- chessProject/src/boardgame/Board.java
+++ chessProject/src/boardgame/Board.java
@@ -5,7 +5,7 @@ public class Board {
     private int columns;
     private Piece[][] pieces;
 
-    public Board(int rows, int columns, Piece[][] pieces) {
+    public Board(int rows, int columns) {
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns];
@@ -26,4 +26,14 @@ public class Board {
     public void setColumns(int columns) {
         this.columns = columns;
     }
+
+    public Piece piece(int row, int column){
+        return pieces[row][column];
+    }
+
+    public Piece piece(Position position) {
+        return pieces[position.getRow()][position.getColumn()];
+    }
+
+
 }
