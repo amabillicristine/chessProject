99e16438e1304659e6e488b2cd0d0c3f942b9cad
Amabilli Cristine Wizzentainer <115269283+amabillicristine@users.noreply.github.com>
2025-06-09 10:54:35 -0300

    Implement possibleMoves method and enhance UI to display valid moves

--- chessProject/src/chess/ChessMatch.java
+++ chessProject/src/chess/ChessMatch.java
@@ -33,6 +33,12 @@ public class ChessMatch {
         return (ChessPiece) capturedPiece;
     }
 
+    public boolean[][] possibleMoves(ChessPosition sourcePosition) {
+        Position position = sourcePosition.toPosition();
+        validateSourcePosition(position);
+        return board.piece(position).possibleMoves();
+    }
+
     private void validateSourcePosition(Position position) {
         if (!board.thereIsPiece(position)) {
             throw new ChessExeception("There is no piece on source position");
