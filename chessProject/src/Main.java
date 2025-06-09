894f644d6d45f31bb2d37aefac09bd98808a5b3a
Amabilli Cristine Wizzentainer <115269283+amabillicristine@users.noreply.github.com>
2025-06-09 15:24:22 -0300

    Enhance UI to display captured pieces and update match information

--- chessProject/src/Main.java
+++ chessProject/src/Main.java
@@ -4,7 +4,9 @@ import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -13,10 +15,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
+        List<ChessPiece> captured = new ArrayList<>();
         while (true) {
             try {
                 UI.clearScreen();
-                UI.printMatch(chessMatch);
+                UI.printMatch(chessMatch, captured);
                 System.out.println();
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
